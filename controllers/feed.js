7aa0967e134f65419ce4a06762b8f97d7a8e08b5
Alexander Novitsky <novitsky.413x@gmail.com>
2025-10-07 14:31:47 +0300

    385. Deleting Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -32,7 +32,7 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
-    const imageUrl = req.file.path.replace("\\" ,"/");
+    const imageUrl = req.file.path.replace('\\', '/');
     const title = req.body.title;
     const content = req.body.content;
     const post = new Post({
@@ -67,12 +67,13 @@ exports.getPost = (req, res, next) => {
                 throw error;
             }
             res.status(200).json({ message: 'Post fetched.', post: post });
-        }).catch((err) => {
+        })
+        .catch((err) => {
             if (!err.statusCode) {
                 err.statusCode = 500;
             }
             next(err);
-        })  ;
+        });
 };
 
 exports.updatePost = (req, res, next) => {
@@ -87,7 +88,7 @@ exports.updatePost = (req, res, next) => {
     const content = req.body.content;
     let imageUrl = req.body.image;
     if (req.file) {
-        imageUrl = req.file.path.replace("\\" ,"/");
+        imageUrl = req.file.path.replace('\\', '/');
     }
     if (!imageUrl) {
         const error = new Error('No image provided.');
@@ -117,10 +118,34 @@ exports.updatePost = (req, res, next) => {
                 err.statusCode = 500;
             }
             next(err);
-        })
+        });
 };
 
-const clearImage = (filePath) => { 
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then((post) => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            // Check logged in user
+            clearImage(post.imageUrl);
+            return Post.deleteOne({ _id: postId, creator: req.userId });
+        })
+        .then((result) => {
+            res.status(200).json({ message: 'Post deleted.' });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
+
+const clearImage = (filePath) => {
     if (!filePath) {
         return; // Don't try to delete if no file path provided
     }
@@ -130,4 +155,4 @@ const clearImage = (filePath) => {
             console.log('Error deleting file:', err);
         }
     });
-}
+};
